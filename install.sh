a02513fce00554a8e9f8f7bdb1a3d9ced2ec1970
Hugo Marin <hmarin@amiltone.com>
2024-07-01 10:30:46 +0200

    using symlinks rather than just mv folder

--- install.sh
+++ install.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-# Move files from dotfiles folder to .config folder for this user
-cp -r dotfiles/* ~/.config/
+# Define the source directory
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "current script directory: $SCRIPT_DIR"
+
+# Define the target directory
+TARGET_DIR="$HOME/.config"
+
+for item in "$SCRIPT_DIR"/dotfiles/*; do
+    # Check if the current item is a directory
+    if [ -d "$item" ]; then
+        # Extract the relative path of the item
+        rel_path=$(basename "$item")
+        
+        # Construct the full path for the symbolic link
+        link_path="$TARGET_DIR/$rel_path"
+        
+        # Remove the existing symbolic link if it exists
+        rm -rf "$link_path" || true
+        
+        # Create the symbolic link
+        ln -s "$item" "$link_path"
+        
+        echo "Created symbolic link: $link_path"
+    fi
+done
